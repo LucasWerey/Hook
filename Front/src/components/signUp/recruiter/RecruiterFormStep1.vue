1adbafb1836bd733be9ffae02addd054294f5521
LucasWerey <lucas.werey@hotmail.com>
2024-01-20 23:02:00 +0100

    fix(front): body view and bg

--- Front/src/components/signUp/recruiter/RecruiterFormStep1.vue
+++ Front/src/components/signUp/recruiter/RecruiterFormStep1.vue
@@ -1,12 +1,7 @@
 <template>
   <div>PH1</div>
-  <button @click="handleSubmit">Next</button>
 </template>
 
 <script setup lang="ts">
 const emit = defineEmits(['submit'])
-
-const handleSubmit = () => {
-  emit('submit')
-}
 </script>
